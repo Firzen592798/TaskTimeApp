a50b60fb670c47db6c10204bc3fb8bd51331ca0e
Firzen592798 <tyranitar_nb@hotmail.com>
2017-02-15 22:19:07 -0300

    salvando imagem na galeria

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController, AlertController } from 'ionic-angular';
 
-import { Camera,  SQLite } from 'ionic-native'; 
+import { Camera,  SQLite, Base64ToGallery } from 'ionic-native'; 
 
 @Component({
   selector: 'page-page1',
@@ -12,7 +12,7 @@ export class Page1 {
   database: SQLite;
   txtTarefa: string;
   cameraData: string;
-
+  imageData: string;
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
     this.database = new SQLite();
     this.database.openDatabase({name: "tasktime.db", location: "default"}).then(() => {
@@ -25,6 +25,10 @@ export class Page1 {
     this.database.executeSql("insert into tarefa (nome, tempo) values('"+this.txtTarefa+"', 0)", []).then((data) => {
       console.log("inserido com sucesso: " +this.txtTarefa);
       console.log(data);
+      Base64ToGallery.base64ToGallery(this.imageData, 'img_').then(
+        res => console.log('Saved image to gallery ', res),
+        err => console.log('Error saving image to gallery ', err)
+      );
       this.showAlert();
     }, (error) => {
       console.log(error);
@@ -49,9 +53,14 @@ export class Page1 {
       encodingType: Camera.EncodingType.JPEG,
       saveToPhotoAlbum: false,
       allowEdit: true, 
+      targetWidth: 192,
+      targetHeight: 256,
+      correctOrientation: true,
     };
     Camera.getPicture(options).then((imageData) => {
        this.cameraData = 'data:image/jpeg;base64,' + imageData;
+       this.imageData = imageData;
+       console.log(this.cameraData);
     }, (err) => {
       console.log("error");
     });
